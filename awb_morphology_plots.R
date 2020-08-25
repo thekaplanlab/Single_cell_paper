c35d0a7d170b94860a6f4d4f29fed436eb94c3ff
mustafapir <mustafapir29@gmail.com>
2020-08-25 13:53:52 +0300

    Make the code more elegant

--- awb_morphology_plots.R
+++ awb_morphology_plots.R
@@ -2,41 +2,50 @@
 library(readxl)
 library(ggplot2)
 
+awb_data<-function(data){
+  
+  colnames(data)[1]<-"Genotype"
+  data1<-data[,c(1,2)]
+  data2<-data[,c(1,3)]
+  data3<-data[,c(1,4)]
+  data4<-data[,c(1,5)]
+  
+  colnames(data1)[2]<-"Count"
+  colnames(data2)[2]<-"Count"
+  colnames(data3)[2]<-"Count"
+  colnames(data4)[2]<-"Count"
+  
+  data1$Phenotype<-"Normal"
+  data2$Phenotype<-"Fan"
+  data3$Phenotype<-"Extra Branch"
+  data4$Phenotype<-"Backward Projection"
+  
+  data1$Count<-as.numeric(data1$Count)
+  data2$Count<-as.numeric(data2$Count)
+  data3$Count<-as.numeric(data3$Count)
+  data3$Count<-as.numeric(data3$Count)
+  
+  
+  data1$Count_p<-(data1$Count*100)/(data3$Count + data1$Count + data2$Count + data4$Count)
+  data2$Count_p<-(data2$Count*100)/(data3$Count + data1$Count + data2$Count + data4$Count)
+  data3$Count_p<-(data3$Count*100)/(data3$Count + data1$Count + data2$Count + data4$Count)
+  data4$Count_p<-(data4$Count*100)/(data3$Count + data1$Count + data2$Count + data4$Count)
+  
+  data_last<-rbind(data1, data2, data3, data4)
+  data_last$Phenotype<-factor(data_last$Phenotype, levels = unique(data_last$Phenotype))
+  data_last$Genotype<-factor(data_last$Genotype, levels = c(data1$Genotype))
+  return(data_last)
+}
+
 awb<-read_xlsx("./data/AWB_Cilia_Morphology_Analysis.xlsx")
+#awb_2<-read_xlsx("./data/AWB_Cilia_Morphology_Analysis.xlsx",2)
+#awb_3<-read_xlsx("./data/AWB_Cilia_Morphology_Analysis.xlsx",3)
+#awb_4<-read_xlsx("./data/AWB_Cilia_Morphology_Analysis.xlsx",4)
 
-colnames(awb)[1]<-"Genotype"
-awb1<-awb[,c(1,2)]
-awb2<-awb[,c(1,3)]
-awb3<-awb[,c(1,4)]
-awb4<-awb[,c(1,5)]
+#awb_2t<-awb_data(awb_2)
+awb<-awb_data(awb)
 
-colnames(awb1)[2]<-"Count"
-colnames(awb2)[2]<-"Count"
-colnames(awb3)[2]<-"Count"
-colnames(awb4)[2]<-"Count"
-
-awb1$Phenotype<-"Normal"
-awb2$Phenotype<-"Fan"
-awb3$Phenotype<-"Extra Branch"
-awb4$Phenotype<-"Backward Projection"
-
-awb1$Count<-as.numeric(awb1$Count)
-awb2$Count<-as.numeric(awb2$Count)
-awb3$Count<-as.numeric(awb3$Count)
-awb3$Count<-as.numeric(awb3$Count)
-
-
-awb1$Count_p<-(awb1$Count*100)/(awb3$Count + awb1$Count + awb2$Count + awb4$Count)
-awb2$Count_p<-(awb2$Count*100)/(awb3$Count + awb1$Count + awb2$Count + awb4$Count)
-awb3$Count_p<-(awb3$Count*100)/(awb3$Count + awb1$Count + awb2$Count + awb4$Count)
-awb4$Count_p<-(awb4$Count*100)/(awb3$Count + awb1$Count + awb2$Count + awb4$Count)
-
-awb_last<-rbind(awb1, awb2, awb3, awb4)
-awb_last$Phenotype<-factor(awb_last$Phenotype, levels = unique(awb_last$Phenotype))
-awb_last$Genotype<-factor(awb_last$Genotype, levels = c(awb1$Genotype))
-
-
-# Plot for head
+# Plot for first table
 
 xsub0 <- ~ atop(paste(italic("wdr-31"),"(tm10423);",italic("elmod-3")))
 xsub1 <- ~ atop(paste(italic("wdr-31"),"(tm10423); "), paste(italic("elmod-3"), ";Ex[ELMD-1(+)]"))
